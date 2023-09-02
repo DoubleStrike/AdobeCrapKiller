f858cde56da42accfe245367e91cecab6f51914e
VRBOX\GateKeeper <nikhil@doublestrike.com>
2023-09-01 22:02:51 -0400

    Added all processes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -36,6 +36,17 @@ namespace AdobeCrapKiller
             // HACK: This is simple brute force for now, as a concept test
             lblProcess01.FontWeight = ProcessExtensions.IsRunning(lblProcess01.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
             lblProcess02.FontWeight = ProcessExtensions.IsRunning(lblProcess02.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess03.FontWeight = ProcessExtensions.IsRunning(lblProcess03.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess04.FontWeight = ProcessExtensions.IsRunning(lblProcess04.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess05.FontWeight = ProcessExtensions.IsRunning(lblProcess05.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess06.FontWeight = ProcessExtensions.IsRunning(lblProcess06.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess07.FontWeight = ProcessExtensions.IsRunning(lblProcess07.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess08.FontWeight = ProcessExtensions.IsRunning(lblProcess08.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess09.FontWeight = ProcessExtensions.IsRunning(lblProcess09.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess10.FontWeight = ProcessExtensions.IsRunning(lblProcess10.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess11.FontWeight = ProcessExtensions.IsRunning(lblProcess11.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess12.FontWeight = ProcessExtensions.IsRunning(lblProcess12.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess13.FontWeight = ProcessExtensions.IsRunning(lblProcess13.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
         }
     }
 }
