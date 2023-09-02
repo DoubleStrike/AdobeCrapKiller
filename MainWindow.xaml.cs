ff6e08c75d782eecc7b8680a53b541566abb5d4e
VRBOX\GateKeeper <nikhil@doublestrike.com>
2023-09-01 22:10:51 -0400

    Basic process killing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,19 +34,31 @@ namespace AdobeCrapKiller
         private void getProcessStatusTimer_Tick(object sender, EventArgs e)
         {
             // HACK: This is simple brute force for now, as a concept test
-            lblProcess01.FontWeight = ProcessExtensions.IsRunning(lblProcess01.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess02.FontWeight = ProcessExtensions.IsRunning(lblProcess02.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess03.FontWeight = ProcessExtensions.IsRunning(lblProcess03.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess04.FontWeight = ProcessExtensions.IsRunning(lblProcess04.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess05.FontWeight = ProcessExtensions.IsRunning(lblProcess05.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess06.FontWeight = ProcessExtensions.IsRunning(lblProcess06.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess07.FontWeight = ProcessExtensions.IsRunning(lblProcess07.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess08.FontWeight = ProcessExtensions.IsRunning(lblProcess08.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess09.FontWeight = ProcessExtensions.IsRunning(lblProcess09.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess10.FontWeight = ProcessExtensions.IsRunning(lblProcess10.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess11.FontWeight = ProcessExtensions.IsRunning(lblProcess11.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess12.FontWeight = ProcessExtensions.IsRunning(lblProcess12.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
-            lblProcess13.FontWeight = ProcessExtensions.IsRunning(lblProcess13.Content.ToString()) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess01.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess01.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess02.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess02.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess03.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess03.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess04.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess04.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess05.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess05.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess06.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess06.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess07.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess07.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess08.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess08.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess09.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess09.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess10.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess10.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess11.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess11.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess12.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess12.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+            lblProcess13.FontWeight = ProcessExtensions.IsRunning(SafeString(lblProcess13.Content.ToString())) ? FontWeights.Bold : FontWeights.Regular;
+        }
+
+        private void btnKill_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblProcess01.FontWeight == FontWeights.Bold) ProcessExtensions.KillByName(SafeString(lblProcess01.Content.ToString()));
+        }
+
+        private static string SafeString(string? input)
+        {
+            if (!string.IsNullOrWhiteSpace(input)) return input;
+
+            return "";
         }
     }
 }
--- ProcessExtensions.cs
+++ ProcessExtensions.cs
@@ -6,5 +6,25 @@ namespace AdobeCrapKiller
     public static class ProcessExtensions
     {
         public static bool IsRunning(string name) => Process.GetProcessesByName(name).Length > 0;
+
+        public static Process[] GetByName(string name)
+        {
+            return Process.GetProcessesByName(name);
+        }
+
+        public static bool KillByName(string name)
+        {
+            Process[] ProcessList = GetByName(name);
+
+            // Early return if empty
+            if (ProcessList.Length == 0) return false;
+
+            foreach (Process ProcessToKill in ProcessList)
+            {
+                ProcessToKill.Kill();
+            }
+
+            return true;
+        }
     }
 }
