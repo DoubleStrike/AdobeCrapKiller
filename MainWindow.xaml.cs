9a325a4bf31637f706987c423147fcdff079567e
VRBOX\GateKeeper <nikhil@doublestrike.com>
2023-09-01 21:52:09 -0400

    Initial structure

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdobeCrapKiller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            System.Windows.Threading.DispatcherTimer getProcessStatusTimer = new System.Windows.Threading.DispatcherTimer();
+            getProcessStatusTimer.Tick += new EventHandler(getProcessStatusTimer_Tick);
+            getProcessStatusTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            getProcessStatusTimer.Start();
+
+        }
+
+        private void getProcessStatusTimer_Tick(object sender, EventArgs e)
+        {
+            // HACK: This is simple brute force for now, as a concept test
+
+        }
+    }
+}
