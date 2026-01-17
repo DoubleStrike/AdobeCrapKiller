0bf2d90a4c2f3b6a8b3286ee7eea0f705f462eed
Nikhil S. <nikhil@doublestrike.com>
2026-01-17 14:16:05 -0500

    Moved AdobeMemoryWastingCrap to proper standalone file

---
+++ AdobeMemoryWastingCrap.cs
@@ -0,0 +1,16 @@
+﻿namespace AdobeCrapKiller {
+    public class AdobeMemoryWastingCrap {
+        public string ProcessPath { get; set; }
+        public int ProcessId { get; set; }
+
+        public AdobeMemoryWastingCrap(string processPath) {
+            ProcessPath = processPath;
+            ProcessId = 0;
+        }
+
+        public AdobeMemoryWastingCrap(string processPath, int processId) {
+            ProcessPath = processPath;
+            ProcessId = processId;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -208,19 +208,4 @@ namespace AdobeCrapKiller {
         }
 #endregion
     }
-
-    public class AdobeMemoryWastingCrap {
-        public string ProcessPath { get; set; }
-        public int ProcessId { get; set; }
-
-        public AdobeMemoryWastingCrap(string processPath) {
-            ProcessPath = processPath;
-            ProcessId = 0;
-        }
-
-        public AdobeMemoryWastingCrap(string processPath, int processId) {
-            ProcessPath = processPath;
-            ProcessId = processId;
-        }
-    }
 }
