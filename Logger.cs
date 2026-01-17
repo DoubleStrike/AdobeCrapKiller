2db36be545294e0a6c8e7375a8d679e3dca4c84f
Nikhil S. <nikhil@doublestrike.com>
2026-01-16 22:41:28 -0500

    Added basic logger class

---
+++ Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdobeCrapKiller {
+    public static class Logger {
+        const string logFilePath = @"e:\tmp\adobe_crap_killer.log";
+
+        public static void Log(string message) {
+            try {
+                using (var writer = System.IO.File.AppendText(logFilePath)) {
+                    writer.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+                }
+            } catch (Exception) {
+                // Ignore logging errors
+            }
+        }
+    }
+}
